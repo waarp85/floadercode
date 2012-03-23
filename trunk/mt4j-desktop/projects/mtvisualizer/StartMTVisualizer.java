bb069c94b191455bb428bdf34aa4cd92102ff631
arribaud <arribaud@3a27bbb7-097c-c3cb-af54-a38f79fb6de7>
2012-03-23 04:26:04 +0000

    -added osc send

--- trunk/mt4j-desktop/projects/mtvisualizer/StartMTVisualizer.java
+++ trunk/mt4j-desktop/projects/mtvisualizer/StartMTVisualizer.java
@@ -1,5 +1,7 @@
 package mtvisualizer;
 
+import netP5.NetAddress;
+
 import org.mt4j.MTApplication;
 import org.mt4j.AbstractMTApplication.CurrentClassGetter;
 
@@ -13,6 +15,8 @@ import peasy.PeasyCam;
 public class StartMTVisualizer extends MTApplication {
 	private static final long serialVersionUID = 1L;
 	public static final int OSC_PORT = 7400;
+	public static final String OSC_REMOTE_ADDR = "localhost";
+	public static final int OSC_REMOTE_PORT = 7500;
 	
 	OscP5 oscP5;
 	VisualizerScene mainScene;
@@ -20,7 +24,7 @@ public class StartMTVisualizer extends MTApplication {
 	@Override
 	public void startUp() {
 		oscP5 = new OscP5(this, OSC_PORT);
-		mainScene = new VisualizerScene(this, "main scene");
+		mainScene = new VisualizerScene(this, "main scene", oscP5, new NetAddress(OSC_REMOTE_ADDR,OSC_REMOTE_PORT));
 		addScene(mainScene);
 	}
 	
