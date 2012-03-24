bf052f77af80bb54011cb215547c649c2ec689f2
arribaud <arribaud@3a27bbb7-097c-c3cb-af54-a38f79fb6de7>
2012-03-24 21:21:20 +0000

    -switched to Ani library for tweening -updated namespace to floader

---
+++ trunk/visualizer/src/floader/visuals/flyingobjects/AbstractMovingObject.java
@@ -0,0 +1,135 @@
+package floader.visuals.flyingobjects;
+
+import java.util.Iterator;
+
+import de.looksgood.ani.*;
+import processing.core.PApplet;
+import wblut.core.processing.WB_Render;
+import wblut.geom.core.WB_Line;
+import wblut.geom.core.WB_Point3d;
+import wblut.geom.core.WB_Vector3d;
+import wblut.hemesh.core.HE_Face;
+import wblut.hemesh.core.HE_Mesh;
+import wblut.hemesh.core.HE_Selection;
+import wblut.hemesh.modifiers.HEM_Extrude;
+import wblut.hemesh.modifiers.HEM_Twist;
+
+public abstract class AbstractMovingObject {
+	
+	public int distanceMult;
+	public float duration;
+	public boolean isPlaying;
+	protected int yOffset;
+	protected int xOffset;
+	PApplet app;
+	WB_Render render;
+	
+	//effects
+	//effects extrude
+	HEM_Extrude extrude;
+	float effectExtrudeDistanceScale;
+	float effectExtrudeDistanceBase;
+	//effects twistx
+	HEM_Twist twist;
+	float effectTwistXScale;
+	float effectTwistXBase;
+	//effects scale
+	float effectScaleBase;
+	float effectScale;
+	private float zOffset;
+	Ani zOffsetAni;
+	
+	public AbstractMovingObject(int distanceMult, float duration, int yOffset, int xOffset, PApplet app, WB_Render render)
+	{
+		this.distanceMult = distanceMult;
+		this.duration = duration;
+		this.app = app;
+		this.render = render;
+		this.yOffset = yOffset;
+		this.xOffset = xOffset;
+		zOffsetAni = new Ani(this, duration, "zOffset", -2000 * distanceMult);
+	}
+	
+	protected void applyGlobalMovement(HE_Mesh mesh)
+	{
+		mesh.move(xOffset, yOffset, zOffset);
+	}
+	
+	public void play()
+	{
+		zOffsetAni.start();
+		isPlaying = true;
+	}
+	
+	public void stop()
+	{
+		isPlaying = false;
+	}
+	
+	public boolean isPlaying()
+	{
+		return isPlaying;
+	}
+	
+	protected HE_Selection getRandomSelection(int seed, int threshold, HE_Mesh mesh)
+	{
+		 HE_Selection selection = new HE_Selection(mesh);
+		 Iterator <HE_Face> fItr = mesh.fItr();
+		 HE_Face f;
+		 app.randomSeed(seed);
+		 while (fItr.hasNext()) { f = fItr.next(); if (app.random(100) < threshold) { selection.add(f); } }
+		 return selection;
+	}
+	
+	public abstract void draw();
+	
+	//Effects
+	protected void applyGlobalEffects(HE_Mesh mesh)
+	{
+		applyExtrude(mesh);
+		applyTwistX(mesh);
+		applyScale(mesh);
+	}
+	
+	//EXTRUDE
+	public void effectEnableExtrude(float _effectExtrudeDistanceScale)
+	{
+		effectExtrudeDistanceScale = _effectExtrudeDistanceScale;
+	}
+	
+	protected void applyExtrude(HE_Mesh mesh)
+	{
+		if(effectExtrudeDistanceBase * effectExtrudeDistanceScale > 0)
+		{
+			extrude = new HEM_Extrude();
+			extrude.setDistance(effectExtrudeDistanceBase * effectExtrudeDistanceScale);
+			mesh.modify(extrude);
+		}
+	}
+	
+	//TWISTX
+	protected void applyTwistX(HE_Mesh mesh)
+	{
+		if(effectTwistXBase * effectTwistXScale > 0)
+			mesh.modify(new HEM_Twist().setAngleFactor(effectTwistXBase * effectTwistXScale).setTwistAxis(new WB_Line(new WB_Point3d(mesh.getCenter().xf()+10,mesh.getCenter().yf(),mesh.getCenter().zf()), new WB_Vector3d(mesh.getCenter().xf()+1,mesh.getCenter().yf(),mesh.getCenter().zf()))));
+	}
+	
+	public void effectEnableTwistX(float _effectTwistXScale)
+	{
+		effectTwistXScale = _effectTwistXScale;
+	}
+	
+	//SCALE
+	protected void applyScale(HE_Mesh mesh)
+	{
+		//Add one so we don't end up with a scale of 0
+		mesh.scale((effectScale * effectScaleBase) + 1);
+	}
+	
+	public void effectEnableScale(float _effectScale)
+	{
+		effectScale = _effectScale;
+	}
+	
+	
+}
---
+++ trunk/visualizer/src/floader/visuals/flyingobjects/SendConeLayer.java
@@ -0,0 +1,75 @@
+package floader.visuals.flyingobjects;
+
+import java.util.Iterator;
+
+import de.looksgood.ani.*;
+import processing.core.*;
+import wblut.geom.core.*;
+import wblut.hemesh.creators.*;
+import wblut.hemesh.tools.*;
+import wblut.geom.grid.*;
+import wblut.geom.nurbs.*;
+import wblut.core.math.*;
+import wblut.hemesh.subdividors.*;
+import wblut.core.processing.*;
+import wblut.hemesh.composite.*;
+import wblut.core.random.*;
+import wblut.hemesh.core.*;
+import wblut.geom.frame.*;
+import wblut.core.structures.*;
+import wblut.hemesh.modifiers.*;
+import wblut.hemesh.simplifiers.*;
+import wblut.geom.triangulate.*;
+import wblut.geom.tree.*;
+import processing.opengl.*;
+import peasy.*;
+
+public class SendConeLayer extends AbstractMovingObject {
+
+	HE_Mesh cone;
+
+	public SendConeLayer(int _distanceScale, float _duration, int _yOffset, int _xOffset, PApplet _sketch, WB_Render _render) {
+		super(_distanceScale, _duration, _yOffset, _xOffset, _sketch, _render);
+
+		//rotationTween = new Tween("rotation", 0f, 2 * PConstants.PI, 213);
+		
+		effectExtrudeDistanceBase = 20;
+		effectTwistXBase = 10f;
+		effectScaleBase = 10;
+	}
+
+	public void play() {
+		super.play();
+		
+	}
+
+	public void stop() {
+		super.stop();
+	}
+
+	public void draw() {
+		//Layer-specific creation & transforms
+		////
+		HEC_Cone conCreator = new HEC_Cone();
+		conCreator.setRadius(10).setHeight(30).setFacets(4).setSteps(5);
+		cone = new HE_Mesh(conCreator);
+
+		//Rotate to point forwards
+		WB_Point3d p = cone.getCenter();
+		WB_Point3d q = new WB_Point3d(p.x + 1, p.y, p.z);
+		cone.rotateAboutAxis(PConstants.PI, p, q);
+		////
+		
+		applyGlobalMovement(cone);
+		applyGlobalEffects(cone);
+		
+		//abstract this for themes
+		app.fill(198,247,4);
+		
+		app.noStroke();
+		render.drawFaces(cone);
+	}
+	
+
+	
+}
