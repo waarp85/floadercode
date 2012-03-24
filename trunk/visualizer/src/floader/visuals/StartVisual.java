721d31ef13a9f7d1953cfe7895508a610920d868
arribaud <arribaud@3a27bbb7-097c-c3cb-af54-a38f79fb6de7>
2012-03-24 22:57:39 +0000

    -add hang on but performance is slow

--- trunk/visualizer/src/floader/visuals/StartVisual.java
+++ trunk/visualizer/src/floader/visuals/StartVisual.java
@@ -2,6 +2,7 @@ package floader.visuals;
 
 import de.looksgood.ani.Ani;
 import floader.visuals.flyingobjects.FlyingObjectsVisual;
+import floader.visuals.hangon.HangOnVisual;
 import floader.visuals.tearsfordears.TearsForDearsVisual;
 import oscP5.OscMessage;
 import oscP5.OscP5;
@@ -10,7 +11,7 @@ import processing.core.PGraphics;
 
 public class StartVisual extends PApplet {
 
-	TearsForDearsVisual visualizer;
+	HangOnVisual viz;
 	OscP5 oscP5;
 	
 	public static final int OSC_PORT = 7400;
@@ -20,15 +21,15 @@ public class StartVisual extends PApplet {
 		size(1024, 768, OPENGL);
 		oscP5 = new OscP5(this, OSC_PORT);
 		Ani.init(this);
-		visualizer= new TearsForDearsVisual(this);
-		visualizer.setup();
+		viz= new HangOnVisual(this);
+		viz.setup();
 		
 	}
 	
 	public void draw()
 	{
 		background(0,0,0);
-		visualizer.draw();
+		viz.draw();
 	}
 	
 	public static void main(String args[]) {
@@ -37,7 +38,7 @@ public class StartVisual extends PApplet {
 	
 	public void oscEvent(OscMessage msg) {
 		
-		visualizer.oscEvent(msg);
+		viz.oscEvent(msg);
 	}
 	
 }
